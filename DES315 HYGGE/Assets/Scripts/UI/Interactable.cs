57154042e350eb3c025e8da6f49b8e8e5dd3c489
Petrolling <81490948+Petrolling@users.noreply.github.com>
2026-03-05 18:21:43 +0000

    pre test

---
+++ DES315 HYGGE/Assets/Scripts/UI/Interactable.cs
@@ -0,0 +1,79 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Interactable : MonoBehaviour
+{
+    [Header("Interaction")]
+    [SerializeField] private string interactionText = "[FILL IN FIELD]";
+    public InputActionAsset InputActions;
+    private InputAction interactAction;
+
+    private SpriteRenderer sprite;
+    private bool playerInRange;
+    private bool isInteracting;
+    private CharacterUI currUI;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        interactAction = InputActions.FindActionMap("Player").FindAction("Interact");
+    }
+
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+
+
+    void Update()
+    {
+        if (playerInRange && !isInteracting && interactAction.WasPressedThisFrame())
+        {
+            Interact();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponentInParent<Player>())
+        {
+            playerInRange = true;
+            currUI = other.GetComponent<CharacterUI>();
+
+            currUI.pressEUI.SetActive(true);
+            SetOutline(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponentInParent<Player>())
+        {
+            playerInRange = false;
+            isInteracting = false;
+            currUI = other.GetComponent<CharacterUI>();
+
+            currUI.pressEUI.SetActive(false);
+            currUI.dialogueUI.SetActive(false);
+            SetOutline(false);
+        }
+    }
+
+    void Interact()
+    {
+        isInteracting = true;
+
+        currUI.pressEUI.SetActive(false);
+        currUI.dialogueUI.SetActive(true);
+        currUI.dialogueText.text = interactionText;
+    }
+
+    void SetOutline(bool enabled)
+    {
+        if (sprite != null)
+        {
+            sprite.material.SetFloat("_Outline", enabled ? 1f : 0f);
+        }
+    }
+}
