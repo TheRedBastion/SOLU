bc3f686b37fc94f7a65da8536be85051c7e84d80
FraserRD <2200570@uad.ac.uk>
2026-03-10 13:48:36 +0000

    Added all current audio to Wwise in preparation for full integration

--- DES315 HYGGE/DES315 HYGGE_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ DES315 HYGGE/DES315 HYGGE_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,33 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID ARMOREDENEMYATTACK = 1992179679U;
+        static const AkUniqueID BATATTACKS = 3626407789U;
+        static const AkUniqueID BATDAMAGED = 3293333115U;
+        static const AkUniqueID BATSLAIN = 770143801U;
+        static const AkUniqueID CAVESAMBIENCE = 3280349325U;
+        static const AkUniqueID FLORALAMBIENCE = 815341915U;
         static const AkUniqueID GFOOTSTEPS = 375687853U;
+        static const AkUniqueID MOONDAMAGED = 1483277815U;
+        static const AkUniqueID MOONMUSIC1 = 3770854140U;
         static const AkUniqueID SFOOTSTEPS = 3194455577U;
+        static const AkUniqueID SUNDAMAGED = 1689873234U;
+        static const AkUniqueID SUNDAMAGEDCAVE = 1203409809U;
+        static const AkUniqueID SUNMUSIC1 = 1938177371U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace ARMOREDENEMY
+        {
+            static const AkUniqueID GROUP = 2769296183U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace ARMOREDENEMY
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
