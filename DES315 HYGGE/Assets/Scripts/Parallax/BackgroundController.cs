ff391b70b1ae9a88d8a4e5fdd217b4d980f5467b
jake_cockburn <2300929@uad.ac.uk>
2026-03-23 23:04:41 +0000

    Added code to script - jake cockburn 2300929

--- DES315 HYGGE/Assets/Scripts/Parallax/BackgroundController.cs
+++ DES315 HYGGE/Assets/Scripts/Parallax/BackgroundController.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class MainGameParallax : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float startPos;
+    public GameObject cam;
+    public float parallaxEffect;
     void Start()
     {
-        
+        startPos = transform.position.x;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float dist = (cam.transform.position.x * parallaxEffect);// 0 with cam 1 wont move
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
     }
 }
