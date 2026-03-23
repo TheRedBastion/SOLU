5d2f5e270668caa3a9f5109d679cd9d010e1efff
jake_cockburn <2300929@uad.ac.uk>
2026-03-23 23:29:08 +0000

    inf scrolling working

--- DES315 HYGGE/Assets/Scripts/Parallax/BackgroundController.cs
+++ DES315 HYGGE/Assets/Scripts/Parallax/BackgroundController.cs
@@ -2,20 +2,30 @@ using UnityEngine;
 
 public class MainGameParallax : MonoBehaviour
 {
-    private float startPos;
+    private float startPos , leng;
     public GameObject cam;
     public float parallaxEffect;
     void Start()
     {
         startPos = transform.position.x;
-
+        leng = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float dist = (cam.transform.position.x * parallaxEffect);// 0 with cam 1 wont move
+        float dist = (cam.transform.position.x * parallaxEffect);
+        float move = cam.transform.position.x * (1 - parallaxEffect);
 
         transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+        if (move > startPos + leng)
+        {
+            startPos += leng;
+        }
+        else if (move < startPos - leng)
+        {
+            startPos -= leng;
+        }
+
     }
 }
