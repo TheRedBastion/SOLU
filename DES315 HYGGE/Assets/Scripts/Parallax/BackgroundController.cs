7e4ba75cdea116e5001538c41c7686f709130b76
jake_cockburn <2300929@uad.ac.uk>
2026-03-25 21:54:08 +0000

    back most background layer now properly follows char on the y axis   other layers are fucked - jake cockburn 2300929

--- DES315 HYGGE/Assets/Scripts/Parallax/BackgroundController.cs
+++ DES315 HYGGE/Assets/Scripts/Parallax/BackgroundController.cs
@@ -5,6 +5,8 @@ public class MainGameParallax : MonoBehaviour
     private float startPos , leng;
     public GameObject cam;
     public float parallaxEffect;
+
+    public int layer;
     void Start()
     {
         startPos = transform.position.x;
@@ -17,7 +19,16 @@ public class MainGameParallax : MonoBehaviour
         float dist = (cam.transform.position.x * parallaxEffect);
         float move = cam.transform.position.x * (1 - parallaxEffect);
 
-        transform.position = new Vector3(startPos + dist, cam.transform.position.y, transform.position.z);
+
+        if (layer == 1)
+        {
+            transform.position = new Vector3(startPos + dist, cam.transform.position.y + 1, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(startPos + dist, /*cam.*/transform.position.y, transform.position.z);
+        }
+        transform.position = new Vector3(startPos + dist, /*cam.*/transform.position.y, transform.position.z);
         if (move > startPos + leng)
         {
             startPos += leng;
